f09dc922c9812cdfcb454c3ef0234326987ec460
Vishal chouhan <vishalchouhannainpur@gmail.com>
2024-04-14 02:21:21 +0530

    cors-install

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ const express = require('express');
 const router = require('./routes');
 const fs = require('fs');
 const bodyParser = require('body-parser');
-
+const cors = require('cors'); // Import the cors package
 
 const app = express();
 
 const port = 3000;
 
-
 app.use(express.json());
 app.use(bodyParser.json());
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.use(router);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
