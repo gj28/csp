a4a0c04e1fec6e4bff8fcd0c7c24e4a90dc23b0a
Kaushalpohekar <Kaushalpohekar1@gmail.com>
2024-04-18 11:08:34 +0530

    Kaushal

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const router = require('./routes');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const cors = require('cors'); // Import the cors package
+const https = require('https');
+
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/senso.senselive.in/privkey.pem', 'utf8');
+const fullchain = fs.readFileSync('/etc/letsencrypt/live/senso.senselive.in/fullchain.pem', 'utf8');
+const credentials = { key: privateKey, cert: fullchain };
 
 const app = express();
 
@@ -10,12 +16,19 @@ const port = 3050;
 
 app.use(express.json());
 app.use(bodyParser.json());
+app.use('/elkem', router);
 
 // Enable CORS for all routes
 app.use(cors());
 
-app.use(router);
+// app.use(router);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(3050, () => {
+  console.log(`HTTPS server listening on port ${port}`);
 });
