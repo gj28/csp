2a977b52c7059928f16f9032349a2b8bcd77aa5a
Kaushalpohekar <Kaushalpohekar1@gmail.com>
2024-04-24 16:48:55 +0530

    Kaushal

--- elkem_app.js
+++ elkem_app.js
@@ -5,9 +5,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors'); // Import the cors package
 const https = require('https');
 
-// const privateKey = fs.readFileSync('/etc/letsencrypt/live/senso.senselive.in/privkey.pem', 'utf8');
-// const fullchain = fs.readFileSync('/etc/letsencrypt/live/senso.senselive.in/fullchain.pem', 'utf8');
-// const credentials = { key: privateKey, cert: fullchain };
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/senso.senselive.in/privkey.pem', 'utf8');
+const fullchain = fs.readFileSync('/etc/letsencrypt/live/senso.senselive.in/fullchain.pem', 'utf8');
+const credentials = { key: privateKey, cert: fullchain };
 
 const app = express();
 
@@ -17,19 +17,19 @@ app.use(express.json());
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use(bodyParser.json());
-//app.use('/elkem', router);
+app.use('/elkem', router);
 
 // Enable CORS for all routes
 app.use(cors());
 
-app.use('/elkem', router);
+// app.use('/elkem', router);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-
-// const httpsServer = https.createServer(credentials, app);
-
-// httpsServer.listen(3050, () => {
-//   console.log(`HTTPS server listening on port ${port}`);
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
 // });
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(3050, () => {
+  console.log(`HTTPS server listening on port ${port}`);
+});
