1e4de9a341cb68e7c903615cb6e32ceb388d8c9a
Kaushalpohekar <Kaushalpohekar1@gmail.com>
2024-04-29 17:59:35 +0530

    Kaushal Final Deployment

--- elkem_app.js
+++ elkem_app.js
@@ -14,7 +14,6 @@ const app = express();
 const port = 3050;
 app.use(cors());
 
-app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use(bodyParser.json());
 app.use('/elkem', router);
