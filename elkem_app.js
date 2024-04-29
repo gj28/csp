3b0a7579abac39a7de52584244bdabbcef1e35b8
Kaushalpohekar <Kaushalpohekar1@gmail.com>
2024-04-29 17:07:30 +0530

    Kaushal Final Deployment

--- elkem_app.js
+++ elkem_app.js
@@ -1,27 +1,32 @@
 const express = require('express');
-const cors = require('cors');
 const router = require('./routes');
 const fs = require('fs');
 const bodyParser = require('body-parser');
-const https = require('https')
+const cors = require('cors'); // Import the cors package
+const https = require('https');
 
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/senso.senselive.in/privkey.pem', 'utf8');
 const fullchain = fs.readFileSync('/etc/letsencrypt/live/senso.senselive.in/fullchain.pem', 'utf8');
-
-
 const credentials = { key: privateKey, cert: fullchain };
-
 
 const app = express();
 
 const port = 3050;
 
-
-app.use(cors());
+app.use(express.json());
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use(bodyParser.json());
 app.use('/elkem', router);
+
+// Enable CORS for all routes
+app.use(cors());
+
+// app.use('/elkem', router);
+
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
 
 const httpsServer = https.createServer(credentials, app);
 
